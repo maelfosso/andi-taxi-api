7adcca26289c4f3ad2c30f607679f1ae8847ff7d
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-12 04:57:54 +0100

    added User model

---
+++ src/models/User.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema, Types } from 'mongoose';
+
+interface UserAttributes {
+  name: string;
+  phone: string;
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+  build(attrs: UserAttributes): UserDocument;
+}
+
+interface UserDocument extends mongoose.Document {
+  name: string;
+  phone: string;
+  createdAt: Date;
+}
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    validate: [
+      (v:string) => {
+        return /^$\+\d{1,}/.test(v)
+      },
+      '{PATH} must be a valid phone number (+xxxxxxxx)'
+    ]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, { strict: false });
+
+UserSchema.statics.build = (attrs: UserAttributes) => {
+  return new User(attrs);
+}
+
+const User = mongoose.model<UserDocument, UserModel>('User', UserSchema);
+
+export { User, UserDocument, UserAttributes };
